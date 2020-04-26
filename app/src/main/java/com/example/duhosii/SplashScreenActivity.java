0fc961403b10757a4e74548f096f462f6f4896fe
Kreso242 <kresimirtomic1998@gmail.com>
2020-04-26 22:30:16 +0200

    DUH-kreso-001: NavigationBar+ToolBar+Dialog+Splash

---
+++ app/src/main/java/com/example/duhosii/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.duhosii;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Objects;
+
+
+public class SplashScreenActivity extends AppCompatActivity{
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide(); //hide app name bar
+
+        //wait for 5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void run() {
+                SplashScreenActivity.this.startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                SplashScreenActivity.this.finish();
+            }
+        },2600);
+    }
+
+}
