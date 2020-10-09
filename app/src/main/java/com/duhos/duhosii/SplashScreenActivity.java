b7e639090d61564d04711bdaeb9fd7d8cee051ee
Kreso <kresimirtomic1998@mail.com>
2020-10-09 19:48:43 +0200

    DUH-Kreso-037: Popravak kopiranja vremena alarma u kalendaru

--- app/src/main/java/com/duhos/duhosii/SplashScreenActivity.java
+++ app/src/main/java/com/duhos/duhosii/SplashScreenActivity.java
@@ -1,86 +1,48 @@
 package com.duhos.duhosii;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.duhos.duhosii.R;
 
 import java.util.Objects;
 
 import io.realm.Realm;
 
 
-public class SplashScreenActivity extends Activity {
+public class SplashScreenActivity extends AppCompatActivity{
 
-    protected int _splashTime = 5000;
-
-    private Thread splashTread;
-
-    /** Called when the activity is first created. */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash_screen);
-
-
-        final SplashScreenActivity sPlashScreen = this;
-
-        // thread for displaying the SplashScreen
-        splashTread = new Thread() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide(); //hide app name bar
+        Realm.init(getApplicationContext());
+        //wait for 5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void run() {
-                try {
-                    synchronized(this){
-                        wait(_splashTime);
-                    }
-
-                } catch(InterruptedException e) {}
-                finally {
-
-                    if(!isFinishing()) // This pretty useful boolean val tells if
-                    //user has pressed the back button. very useful.
-                    {Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
-
-                        startActivity(i);
-                        finish();
-                    }
-                    onStop();
-                }
+                SplashScreenActivity.this.startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                SplashScreenActivity.this.finish();
             }
-        };
-
-        splashTread.start();
+        },3600);
     }
+
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            Toast.makeText(this,"exec---",Toast.LENGTH_LONG).show();
-            synchronized(splashTread){
-                splashTread.notifyAll();
-            }
-        }
-        return true;
-    }
-    @Override
-    protected void onPause() {
-
-        super.onPause();
-
-        if(splashTread.getState()==Thread.State.TIMED_WAITING){
-            //Thread is still waiting and Activity is paused. Means user has pressed Home. Bail out
-            finish();
-        }
-
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
+        super.onBackPressed();
     }
 }
